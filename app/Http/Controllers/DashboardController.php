615bbb82996c496f2db5ca8b89bf08df510e06a0
Paulo Martins <paulomartinsasd@hotmail.com>
2023-11-08 18:41:10 -0300

    Implementação de Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cargo;
+use App\Models\Departamento;
+use App\Models\Funcionario;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -16,8 +19,19 @@ class DashboardController extends Controller
      */
     public function index(Request $request)
     {
-        // Receber os dados do banco através
-        return view('dashboard.index');
+        $totalFuncionarios = Funcionario::where('status','on')->count();
+        $totalCargos = Cargo::all()->count();
+        $totalDepartamentos = Departamento::all()->count();
+        $somaSalarios = Funcionario::where('status','on')->sum('salario');
+
+         // Dados dos departamentos
+         $departamentos = Departamento::all()->sortBy('nome');
+         foreach($departamentos AS $departamento){
+             $nomesDepartamentos[] = "'".$departamento->nome."'";
+             // $totalFuncionariosDepartamentos[] = $departamento->fun
+         }
+
+         return view('dashboard.index', compact('totalFuncionarios', 'totalCargos', 'totalDepartamentos', 'somaSalarios'));
     }
 
 }
